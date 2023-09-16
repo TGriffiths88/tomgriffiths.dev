b9077a874ed38d34f4d9214dd3156dd4adc9b09d
TGriffiths88 <tom.griffiths@madebymutual.com>
2023-09-16 14:16:56 +0100

    Center text on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,33 +14,35 @@ export default function Home() {
     <ModalProvider transTime={250}>
       <main>
         <Frame imageSrc="https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80">
-          <p className="text-2xl">
-            Hi, I am a <strong>creative typescript developer</strong>, working
-            remotely from Hastings, UK.
-          </p>
-          <p className="text-lg mt-4">
-            I love developing accessible sites and applications that look great
-            and perform well. With a flair for solving problems with creative
-            solutions.
-          </p>
-          <p className="mt-4">
-            For any enquiries please{" "}
-            <ModalToggler slug="contact" className="font-bold underline">
-              message me.
-            </ModalToggler>
-          </p>
-          <SkillList skills={config.skills} className="mt-10" />
-          <p className="text-xs mt-16 block">
-            This site is built with Next.js app router.{" "}
-            <a
-              href="https://github.com/TGriffiths88/tomgriffiths.dev"
-              className="font-bold underline"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              See the code here.
-            </a>
-          </p>
+          <div className="min-h-full flex flex-col justify-center">
+            <p className="text-2xl">
+              Hi, I am a <strong>creative typescript developer</strong>, working
+              remotely from Hastings, UK.
+            </p>
+            <p className="text-lg mt-4">
+              I love developing accessible sites and applications that look
+              great and perform well. With a flair for solving problems with
+              creative solutions.
+            </p>
+            <p className="mt-4">
+              For any enquiries please{" "}
+              <ModalToggler slug="contact" className="font-bold underline">
+                message me.
+              </ModalToggler>
+            </p>
+            <SkillList skills={config.skills} className="mt-10" />
+            <p className="text-xs mt-16 block">
+              This site is built with Next.js app router.{" "}
+              <a
+                href="https://github.com/TGriffiths88/tomgriffiths.dev"
+                className="font-bold underline"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                See the code here.
+              </a>
+            </p>
+          </div>
         </Frame>
       </main>
       <ContactModal />
