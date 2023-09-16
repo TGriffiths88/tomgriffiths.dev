0734d8f23c50cc90622262cf3e66c49df6187e5f
TGriffiths88 <tom.griffiths@madebymutual.com>
2023-09-16 11:22:12 +0100

    Add copyright to footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -23,8 +23,8 @@ const FOOTER_LINKS = [
 
 const Footer = ({}: FooterProps) => {
   return (
-    <footer className="bg-black text-white lg:bg-transparent lg:mix-blend-difference lg:fixed lg:bottom-0 lg:w-full">
-      <ul className="uppercase tracking-wide flex text-xs space-x-6 justify-center py-10 px-4 md:px-10 lg:justify-start lg:py-6">
+    <footer className="bg-black z-10 text-white lg:bg-transparent lg:mix-blend-difference lg:fixed lg:bottom-0 lg:left-0 lg:pb-4 lg:px-10 lg:right-0 py-10">
+      <ul className="uppercase tracking-wide flex text-xs space-x-6 justify-center mb-4 lg:mb-0 px-4 md:px-10 lg:justify-start lg:py-6 lg:pb-0 lg:px-0">
         {FOOTER_LINKS.map((link) => (
           <li key={link.href}>
             <a
@@ -37,6 +37,9 @@ const Footer = ({}: FooterProps) => {
           </li>
         ))}
       </ul>
+      <small className="text-xs opacity-50 text-center w-full block lg:text-left">
+        &copy;{new Date().getFullYear()} Tom Griffiths
+      </small>
     </footer>
   );
 };
