ba7c973360d7dc027f203169ffb45b661bad4081
TGriffiths88 <tom.griffiths@madebymutual.com>
2023-09-16 14:27:04 +0100

    Update modal styling and schema message

--- src/components/ui/modal/index.tsx
+++ src/components/ui/modal/index.tsx
@@ -12,28 +12,29 @@ const UiModal = ({ children, slug, onClose }: ModalProps) => {
   return (
     <Modal
       slug={slug}
-      className="rounded-md p-5 md:px-8 py-12 shadow-lg w-[calc(100vw-30px)] max-w-md relative bg-white"
+      className="rounded-md p-5 md:p-8 pt-12 shadow-lg w-[calc(100vw-30px)] max-w-md relative bg-white"
       onClose={onClose}
     >
-      <div className="">
-        <ModalToggler className="absolute top-5 right-5" slug={slug}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6 opacity-50 hover:opacity-100 duration-300"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </ModalToggler>
-        {children}
-      </div>
+      <ModalToggler
+        className="absolute top-5 right-5 md:top-6 md:right-6"
+        slug={slug}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 opacity-50 hover:opacity-100 duration-300"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </ModalToggler>
+      {children}
     </Modal>
   );
 };
--- src/schemas/contact-form.ts
+++ src/schemas/contact-form.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 
 export const ContactFormSchema = z.object({
-  name: z.string({ required_error: "You must enter a name" }).min(1),
+  name: z
+    .string({ required_error: "You must enter a name" })
+    .min(1, { message: "You must enter a name" }),
   email: z
     .string({ required_error: "You must enter an email" })
     .email({ message: "You must enter a valid email" }),
