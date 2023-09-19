92afd171058c5dc1d075c03c7f90dc91d064ac28
TGriffiths88 <tom.griffiths@madebymutual.com>
2023-09-19 17:24:37 +0100

    Update site footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,8 +5,8 @@ type FooterProps = {};
 
 const Footer = ({}: FooterProps) => {
   return (
-    <footer className="bg-black z-10 text-white lg:bg-transparent lg:mix-blend-difference lg:fixed lg:bottom-0 lg:left-0 lg:pb-4 lg:px-10 lg:right-0 py-10">
-      <ul className="uppercase tracking-wide flex text-xs space-x-6 justify-center mb-4 lg:mb-2 px-4 md:px-10 lg:justify-start lg:py-6 lg:pb-0 lg:px-0">
+    <footer className="bg-black z-10 text-white lg:bg-transparent lg:mix-blend-difference lg:fixed lg:bottom-0 lg:left-0 lg:pb-4 lg:px-10 lg:right-0 py-10 lg:flex lg:items-center">
+      <ul className="uppercase tracking-wide flex text-xs space-x-6 justify-center mb-4 lg:mb-2 px-4 md:px-10 lg:justify-start lg:py-6 lg:pb-0 lg:px-0 lg:pt-0">
         {config.footerLinks.map((link) => (
           <li key={link.href}>
             <a
@@ -19,7 +19,7 @@ const Footer = ({}: FooterProps) => {
           </li>
         ))}
       </ul>
-      <small className="text-xs opacity-50 text-center w-full block lg:text-left">
+      <small className="text-xs opacity-50 lg:opacity-25 text-center w-full block lg:text-right">
         &copy;{new Date().getFullYear()} Tom Griffiths
       </small>
     </footer>
