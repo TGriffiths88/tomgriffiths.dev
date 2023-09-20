d99eb935879f8255f67f5bc96962c9ecd635fde3
TGriffiths88 <tom.griffiths@madebymutual.com>
2023-09-20 08:46:31 +0100

    Update footer nav item animation

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,15 +6,37 @@ import { motion } from "framer-motion";
 type FooterProps = {};
 
 const Footer = ({}: FooterProps) => {
+  const footerVariants = {
+    hidden: {},
+    visible: {
+      transition: {
+        delayChildren: 1.4,
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const footerItemVariants = {
+    hidden: {
+      opacity: 0,
+      y: 20,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
   return (
     <motion.footer
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { delay: 1.4 } }}
+      variants={footerVariants}
+      initial="hidden"
+      animate="visible"
       className="bg-black z-10 text-white lg:bg-transparent lg:mix-blend-difference lg:fixed lg:bottom-0 lg:left-0 lg:pb-4 lg:px-10 lg:right-0 py-10 lg:flex lg:items-center"
     >
-      <ul className="uppercase tracking-wide flex text-xs space-x-6 justify-center mb-4 lg:mb-2 px-4 md:px-10 lg:justify-start lg:py-6 lg:pb-0 lg:px-0 lg:pt-0">
+      <motion.ul className="uppercase tracking-wide flex text-xs space-x-6 justify-center mb-4 lg:mb-2 px-4 md:px-10 lg:justify-start lg:py-6 lg:pb-0 lg:px-0 lg:pt-0">
         {config.footerLinks.map((link) => (
-          <li key={link.href}>
+          <motion.li key={link.href} variants={footerItemVariants}>
             <a
               href={link.href}
               rel="noopener noreferrer"
@@ -22,9 +44,9 @@ const Footer = ({}: FooterProps) => {
             >
               {link.title}
             </a>
-          </li>
+          </motion.li>
         ))}
-      </ul>
+      </motion.ul>
       <small className="text-xs opacity-50 lg:opacity-25 text-center w-full block lg:text-right">
         &copy;{new Date().getFullYear()} Tom Griffiths
       </small>
