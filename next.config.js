3af988f7abc8aec42844b65953565349ff327c2f
TGriffiths88 <tom.griffiths@madebymutual.com>
2023-09-22 08:36:30 +0100

    Add redirects

--- next.config.js
+++ next.config.js
@@ -3,6 +3,30 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
+  redirects: async () => {
+    return [
+      {
+        source: "/services/:slug*",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/portfolio",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/philosophy",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/contact",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
