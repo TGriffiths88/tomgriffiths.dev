4f351286cfcfce309c96a42e2d3b8ca114f18a88
TGriffiths88 <tom.griffiths@madebymutual.com>
2023-10-13 15:55:07 +0100

    Update site header style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -8,13 +8,13 @@ type HeaderProps = {};
 const Header = ({}: HeaderProps) => {
   return (
     <motion.header
-      className="p-4 md:px-10 md:py-6 fixed top-0 w-full z-50"
+      className="p-4 md:px-10 md:py-6 fixed top-0 w-full z-50 flex"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { delay: 1 } }}
     >
       <Link
         href="/"
-        className="font-black text-black text-2xl hover:opacity-70 duration-300"
+        className="font-black text-black text-lg hover:opacity-70 duration-300  ml-auto"
       >
         TOM GRIFFITHS
       </Link>
